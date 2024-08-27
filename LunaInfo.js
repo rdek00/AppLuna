f458caff4a98abd99683756bd4d8ae573ce48257
rdek00 <116583484+rdek00@users.noreply.github.com>
2024-08-28 00:10:34 +0300

    Update LunaInfo.js

--- LunaInfo.js
+++ LunaInfo.js
@@ -1,6 +1,7 @@
 const PhaseURL = 'https://api.farmsense.net/v1/moonphases/?d=';
 const today = Math.floor(new Date().getTime() / 1000);
-const completeday = 29.5;
+const completeday = 29.53059027777777;
+
 async function getLunarPhase() {
   const response = await fetch(PhaseURL + today);
   const data = await response.json();
@@ -8,12 +9,27 @@ async function getLunarPhase() {
 }
 
 getLunarPhase().then(phase => {
-  console.log('Phase:',phase.Phase);
-  console.log('Distance:',phase.Distance,'kilometers')
-  console.log('Illumination: %' + phase.Illumination * 100);
-  console.log('Quarter age:', Math.floor(phase.Age), 'days');
-  console.log('Remaining days of the quarter:', Math.floor(completeday - phase.Age), 'days');
-  console.log('Quarter percentage: %' + Math.floor((phase.Age / completeday) * 100));
+  console.log('Phase:', phase.Phase);
+  if (phase.Phase == 'Waning Crescent') {
+    console.log('Next phase: New Moon');
+  } else if (phase.Phase == 'New Moon') {
+    console.log('Next phase: Waxing Crescent');
+  } else if (phase.Phase == 'Waxing Crescent') {
+    console.log('Next phase: First Quarter');
+  } else if (phase.Phase == 'Waxing Gibbous') {
+    console.log('Next phase: Full Moon');
+  } else if (phase.Phase == 'Full Moon') {
+    console.log('Next phase: Waning Gibbous');
+  } else if (phase.Phase == 'Waning Gibbous') {
+    console.log('Next phase: Third Quarter');
+  } else if (phase.Phase == 'Third Quarter') {
+    console.log('Next phase: Waning Crescent')
+  }
+  console.log('Distance:', phase.Distance, 'kilometers');
+  console.log('Illumination:', phase.Illumination * 100 + '%');
+  console.log('Period age:', Math.floor(phase.Age), 'days');
+  console.log('Remaining days of the period:', Math.floor(completeday - phase.Age), 'days');
+  console.log('Period percentage (Approximately):', (phase.Age / completeday * 100).toFixed(2) + '%');
 }).catch(error => {
-  console.log(error);
+  console.error('Error fetching lunar phase:', error);
 });
